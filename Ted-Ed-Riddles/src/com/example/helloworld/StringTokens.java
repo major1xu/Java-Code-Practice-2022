912db411c662207e3bedb604d43d65fcc56f3418
major1xu <minjie.xu@gmail.com>
2026-04-17 14:22:47 -0500

    add some comments

--- Ted-Ed-Riddles/src/com/example/helloworld/StringTokens.java
+++ Ted-Ed-Riddles/src/com/example/helloworld/StringTokens.java
@@ -1,6 +1,8 @@
 package com.example.helloworld;
 
 /*
+https://www.hackerrank.com/challenges/java-string-tokens/problem?isFullScreen=true
+
 Given a string, , matching the regular expression [A-Za-z !,?._'@]+, split the string into tokens. We define a token to be one or more consecutive English alphabetic letters. Then, print the number of tokens, followed by each token on a new line.
 
 Note: You may find the String.split method helpful in completing this challenge.
